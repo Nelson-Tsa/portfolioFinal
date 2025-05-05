d94f0bd5cda261b933221862a7504e2301d33da6
Nelson Tsa <t.nelson@hotmail.fr>
2025-05-05 09:58:13 +0200

    ajout d'analyse visiteur avec versel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import {Anek_Telugu} from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react"
 
 const AnekTelugu = Anek_Telugu({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body
         className={cn(GeistSans.variable, GeistMono.variable, AnekTelugu, "font-sans h-full bg-background text-foreground")}>{children}
+        <Analytics />
       </body>
     </html>
   );
